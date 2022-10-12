e7872a97aa78b0fb2d0ca7e4a8814e1eaee06ec9
Daniel Herman <dherman@factset.com>
2022-10-12 16:04:37 -0400

    feat: track both cash and credit prices

--- main.go
+++ main.go
@@ -16,11 +16,55 @@ import (
 )
 
 var (
-	lowestPrice = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "oil_lowest_price",
-		Help: "The lowest price per gallon available in USD",
+	lowestCashPrice = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "oil_lowest_price_cash",
+		Help: "The lowest cash price per gallon available in USD",
+	})
+
+	lowestCreditPrice = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "oil_lowest_price_credit",
+		Help: "The lowest credit price per gallon available in USD",
 	})
 )
+
+func getLowestPriceFromSelector(doc *goquery.Document, selector string) (float64, error) {
+	selection := doc.Find(selector)
+
+	if selection.Length() == 0 {
+		return 0, fmt.Errorf("failed to find selector in document: %v", selector)
+	}
+
+	var prices []float64
+
+	var err error
+
+	selection.Each(func(i int, s *goquery.Selection) {
+		var price float64
+
+		contents := s.Text()
+		price, err = strconv.ParseFloat(contents[1:], 32)
+
+		if err != nil {
+			logrus.Errorf("failed to convert text to price: %v", err)
+			return
+		}
+
+		price = math.Round(price*100) / 100
+		prices = append(prices, price)
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	min := math.MaxFloat64
+
+	for _, price := range prices {
+		min = math.Min(min, price)
+	}
+
+	return min, nil
+}
 
 func recordMetrics(scrapeURL string) {
 	logrus.Debug("scraping url")
@@ -45,26 +89,23 @@ func recordMetrics(scrapeURL string) {
 		return
 	}
 
-	selection := doc.Find("table.paywithcash tr:nth-child(3) td:last-child")
-
-	if selection.Length() == 0 {
-		logrus.Error("failed to find row in response document")
-		return
-	}
-
-	contents := selection.First().Text()
-	price, err := strconv.ParseFloat(contents[1:], 32)
+	cashPrice, err := getLowestPriceFromSelector(doc, "table.paywithcash tr:nth-child(3) td:last-child")
 
 	if err != nil {
-		logrus.Errorf("failed to convert text to price: %v", err)
-		return
+		logrus.Errorf("failed to find low cash price: %v", err)
+	} else {
+		logrus.Infof("low cash price found: %v", cashPrice)
+		lowestCashPrice.Set(cashPrice)
 	}
 
-	price = math.Round(price*100) / 100
+	creditPrice, err := getLowestPriceFromSelector(doc, "table.paybycredit tr:nth-child(3) td:last-child")
 
-	logrus.Infof("low price found: %v", price)
-
-	lowestPrice.Set(price)
+	if err != nil {
+		logrus.Errorf("failed to find low credit price: %v", err)
+	} else {
+		logrus.Infof("low credit price found: %v", creditPrice)
+		lowestCreditPrice.Set(creditPrice)
+	}
 }
 
 func main() {
